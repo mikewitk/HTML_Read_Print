b90ba3efd670a8afa9a2198eaa44ccbe5974b510
Michael <mike.witk@gmail.com>
2019-04-08 15:41:35 +0000

    Implemented getAnd PrintHTML function

---
+++ printHTML.js
@@ -0,0 +1,32 @@
+function getAndPrintHTML () {
+
+  var fullData = '';
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+// notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+https.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    fullData += data + '\n';
+  });
+
+  console.log(data + '\n');
+
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
+
+}
