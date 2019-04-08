b9c98568d6e29cb30e01101f42e1f840111a666f
Michael <mike.witk@gmail.com>
2019-04-08 17:04:48 +0000

    Fixed Step 3

--- getAndPrintParam.js
+++ getAndPrintParam.js
@@ -1,35 +1,36 @@
 function getAndPrintHTML (options) {
 
+  var https = require('https');
   var fullData = '';
-  //Maybe make newOptions keys as the options.host and options.path
-  var newOptions = options;
+  var newOptions = {
+    host: options.host,
+    path: options.path
+  };
 
-  // notice that https.get takes a callback with one parameter -
+// notice that https.get takes a callback with one parameter -
 // response, which is a Stream that represents the HTTP response
-https.get(newOptions, function (response) {
+  https.get(newOptions, function (response) {
 
   // set encoding of received data to UTF-8
   response.setEncoding('utf8');
 
   // the callback is invoked when a `data` chunk is received
   response.on('data', function (data) {
-    fullData += data + '\n';
+    fullData += data;
   });
-
-  console.log(data + '\n');
-
 
   // the callback is invoked when all of the data has been received
   // (the `end` of the stream)
   response.on('end', function() {
-    console.log('Response stream complete.');
+    console.log('Full Data: ', fullData) ;
+    console.log('**------Response stream complete.------**');
   });
-
 });
 }
 
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step2.html'
-  };
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
 
+console.log(getAndPrintHTML(requestOptions)) ;
